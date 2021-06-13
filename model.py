faac8ac74418aabea549d68f614485d9047f7985
neko <mixupup@gmail.com>
2021-06-13 19:12:53 +0000

    New model

--- model.py
+++ model.py
@@ -50,12 +50,69 @@ class CompoResnet(nn.Module):
     resnet.fc = Identity()
     self.resnet = resnet
     
-    MLP = partial(HalvingMLP, num_layers=num_mlp_layers)
-    self.obj_fc = MLP(in_features, OBJ_CLASS)
-    self.attr_fc = MLP(in_features, ATTR_CLASS)
+    self.fc = HalvingMLP(in_features, 800, num_layers=num_mlp_layers)            
+    self.obj_fc = HalvingMLP(400, OBJ_CLASS, 1)
+    self.attr_fc = HalvingMLP(400, ATTR_CLASS, 1)
 
   def forward(self, x):
     img_features = self.resnet(x)
-    obj_pred = self.obj_fc(img_features)
-    attr_pred = self.attr_fc(img_features)
+    features = self.fc(img_features)
+    obj_pred = self.obj_fc(features[:, :400])
+    attr_pred = self.attr_fc(features[:, 400:])
     return obj_pred, attr_pred
+  
+class CompoResnetFeatfile(nn.Module):
+  def __init__(self, resnet_name, num_mlp_layers):
+    super(CompoResnetFeatfile, self).__init__()
+    in_features = 512
+    resnet.fc = Identity()
+    self.resnet = resnet
+    
+    self.fc = HalvingMLP(in_features, 800, num_layers=num_mlp_layers)            
+    self.obj_fc = HalvingMLP(400, OBJ_CLASS, 1)
+    self.attr_fc = HalvingMLP(400, ATTR_CLASS, 1)
+
+  def forward(self, img_features):
+    features = self.fc(img_features)
+    obj_pred = self.obj_fc(features[:, :400])
+    attr_pred = self.attr_fc(features[:, 400:])
+    return obj_pred, attr_pred
+  
+class CompoResnetv2(nn.Module):
+  def __init__(self, resnet_name, num_mlp_layers=2):
+    super(CompoResnetv2, self).__init__()
+    resnet = frozen(torch.hub.load('pytorch/vision:v0.9.0', resnet_name, pretrained=True))
+    in_features = resnet.fc.in_features # 2048 for resnet101
+    resnet.fc = Identity()
+    self.resnet = resnet
+    
+    self.fc = HalvingMLP(in_features, 600, num_layers=num_mlp_layers)            
+    self.obj_fc = HalvingMLP(400, OBJ_CLASS, 1)
+    self.attr_fc = HalvingMLP(200, ATTR_CLASS, 1)
+
+  def forward(self, x):
+    img_features = self.resnet(x)
+    features = self.fc(img_features)
+    obj_pred = self.obj_fc(features[:, :400])
+    attr_pred = self.attr_fc(features[:, 400:])
+    return obj_pred, attr_pred
+  
+class CompoResnetv3(nn.Module):
+  def __init__(self, resnet_name, num_mlp_layers):
+    super(CompoResnetv3, self).__init__()
+    resnet = frozen(torch.hub.load('pytorch/vision:v0.9.0', resnet_name, pretrained=True))
+    in_features = resnet.fc.in_features # 2048 for resnet101
+    resnet.fc = Identity()
+    self.resnet = resnet
+    
+    self.fc = HalvingMLP(in_features, 800, num_layers=num_mlp_layers)            
+    self.obj_fc = HalvingMLP(400, OBJ_CLASS, 1)
+    self.attr_fc = nn.Linear(400, ATTR_CLASS, bias=False)
+
+  def forward(self, x):
+    img_features = self.resnet(x)
+    features = self.fc(img_features)
+    obj_pred = self.obj_fc(features[:, :400])
+    attr_pred = self.attr_fc(features[:, 400:])
+    return obj_pred, attr_pred
+  
