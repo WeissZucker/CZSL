e15e2cac86db3d6ad2752c5834b13b1ee7607029
neko <mixupup@gmail.com>
2021-09-10 09:04:11 +0000

    Add IREvalautor for image retrieval model

--- evaluator.py
+++ evaluator.py
@@ -264,3 +264,35 @@ class EvaluatorWithFscore(Evaluator):
     
     self.attr_labels, self.obj_labels = None, None # drop labels of old batch
     return report
+  
+
+
+class IREvaluator():
+  def __init__(self, cpu_eval):
+    self.dev = 'cpu' if cpu_eval else dev
+    
+  def acc(self, preds, labels):
+    # preds: [nsample, k]
+    nsample = len(preds)
+    labels = labels.unsqueeze(1)
+    acc = (preds == labels).any(dim=1).float().mean()
+    return acc
+  
+  def eval_output(self, output, attr_labels, obj_labels, topk=1):
+    attr_labels = attr_labels.to(self.dev)
+    obj_labels = obj_labels.to(self.dev)
+
+    output = list(zip(*output))
+    img_feats = torch.cat(output[0])
+    queries = torch.cat(output[1])
+    s_pairs = torch.cat(output[2])
+    t_pairs = torch.cat(output[3])
+    
+    dist = torch.cdist(queries, img_feats)
+    predictions = dist.topk(topk, dim=1).indices # nsample * topk
+    pred_pairs = s_pairs[predictions]
+    
+    acc = self.acc(pred_pairs, t_pairs)
+    report = {'IR_Acc': acc.item()}
+    return report
+    
